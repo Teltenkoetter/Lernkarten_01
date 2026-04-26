cc3731529d2f6412e74c56a97bbc06151e8f788e
Teltenkoetter <52072451+Teltenkoetter@users.noreply.github.com>
2026-04-26 13:08:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lernkarten-v18';
+const CACHE = 'memopix-v20';
 const SHELL = ['./index.html', './style.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
